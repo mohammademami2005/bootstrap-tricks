2c0367a69c9a3cdace1be8d02fabd44b5d874a73
{MohammadEmami} <162024353+mohammademami2005@users.noreply.github.com>
2025-08-01 21:08:38 +0330

    update

--- main.js
+++ main.js
@@ -4,14 +4,16 @@ import './style/master.css';
 import './style/carousel.css';
 import './style/offcanvas.css';
 
-import { alert } from './src/alert.js';
-import { carousel } from './src/carousel.js';
-import { accordion } from './src/accordion.js';
-import { offcanvas } from './src/offcanvas.js';
+import { alert } from './src/alert';
+import { carousel } from './src/carousel';
+import { accordion } from './src/accordion';
+import { offcanvas } from './src/offcanvas';
 
-document.addEventListener("DOMContentLoaded", () => {
-  alert();
-  carousel();
+// document.addEventListener("DOMContentLoaded", () => {
+//   alert();
+//   carousel();
   accordion();
   offcanvas();
-});
+// });
+alert()
+carousel()
